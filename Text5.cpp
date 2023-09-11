c9d92b9be9e7450d3da561d124b6742fe18dd388
KuchinaMA <142692420+KuchinaMA@users.noreply.github.com>
2023-09-11 15:17:42 +0300

    Created functions, counted lines

--- Text5.cpp
+++ Text5.cpp
@@ -5,50 +5,82 @@
 
 #include "TXLib.h"
 
-const int MAX_NUM_OF_INES = 100000;
+int size_of_file(FILE * fp);
+int number_of_lines(const char *str, size_t len);
+void create_pointer_arr(char **text, char * buf, int sz);
 
 int main() {
-    FILE * Onegin = fopen( "Onegin.txt" , "r" );
+    FILE * fp = fopen( "Onegin.txt" , "rb" );
 
-    if (Onegin == NULL) {
+    if (fp == NULL) {
         printf("There's no file with text of poem\n");
     }
 
-    fseek(Onegin, 0, SEEK_END);
+    int sz = size_of_file(fp);
+
+    char *buf = (char*) calloc(sz + 1, sizeof(char));
+
+    int nRead = fread(buf, sizeof(char), sz, fp);
+    assert (nRead == sz);
+
+    printf("Размер файла: посчитанный sz = %d и полученный через fread nRead = %d\n\n", sz, nRead);
+
+    fclose(fp);
+
+    int nLines = number_of_lines(buf, sz);
+    printf("Количество строк nLines = %d\n\n", nLines);
+
+    char *text[nLines];
+    for(int i = 0;i < nLines; i++) {
+        text[i] = 0;
+    }
+
+    create_pointer_arr(text, buf, sz);
+
+
+    printf("Вывод текста через массив указателей\n\n");
+    for (int i = 0; i < nLines; ++i) {
+        printf("%s\n", text[i]);
+    }
+    printf("\n");
+    printf("Вывод одной из строк через массив указателей\n");
+    printf("%s\n\n", text[1]);
+
+    printf("Вывод начала текста в буфере...\n");
+    printf("%c", buf[1]);
+    for (int i = 0; i < 10; ++i)
+    {
+        printf("%c", buf[i]);
+    }
+    free(buf);
+    return 0;
+}
+
+
+int size_of_file(FILE * fp) {
+    fseek(fp, 0, SEEK_END);
     int sz = 0;
-    sz = ftell(Onegin);
-    fseek(Onegin, 0, SEEK_SET);
-    //printf("%d\n", sz);
+    sz = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    return sz;
+}
 
-    char *buf = (char*) calloc(sz, sizeof(char));
+int number_of_lines(const char * buf, size_t len) {
+    int nStrings = 0;
+    for (size_t i = 0; i < len; ++i) {
+        nStrings += (buf[i] == '\n');
+    }
+    return nStrings + 1;
+}
 
-    fread(buf, sizeof(char), sz, Onegin);
-
-    fclose(Onegin);
-
-    char *text[MAX_NUM_OF_INES] = {};
+void create_pointer_arr(char ** text, char * buf, int sz) {
     text[0] = buf;
     int line = 1;
     for(int i = 0; i < sz; ++i) {
         if (buf[i] == '\n') {
             buf[i] = '\0';
             text[line] = buf + i + 1;
-            line +=1;
+            line += 1;
         }
     }
-
-    /*for (int i = 0; i < 10; ++i) {
-        printf("%s\n", text[i]);
-    }
-
-    printf("%s\n", text[1]);*/
-
-
-    /*printf("%c", buf[1]);
-    for (int i = 0; i < 10; ++i)
-    {
-        printf("%c", buf[i]);
-    }  */
-    free(buf);
-    return 0;
 }
