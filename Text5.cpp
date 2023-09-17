8f4c3ffa0fc9b6a9adc3969eacc82ce46b5648b4
KuchinaMA <142692420+KuchinaMA@users.noreply.github.com>
2023-09-17 22:41:04 +0300

    Add files via upload

--- Text5.cpp
+++ Text5.cpp
@@ -5,61 +5,58 @@
 #include <ctype.h>
 
 #include "TXLib.h"
+#include "ReadData.h"
+#include "PrintRes.h"
+#include "Sort.h"
+
+/*struct TextData {
+    int sz;
+    char *buf;
+    int nLines;
+    char **text;
+    };*/
 
 
-int size_of_file(FILE * fp);
+/*int size_of_file(FILE * fp);
 int number_of_symbs(const char *str, size_t len, char symb);
 char **create_pointer_arr(char * buf, int sz, int nLines);
-char *read_data_from_file(FILE * fp, int sz);
+char *read_data_from_file(FILE * fp, int sz);*/
 
-void sort_straight(char **text, int nLines);
+/*void sort_straight(char **text, int nLines);
 int comp_lines_straight(const void *line1, const void *line2);
-int strcmp_straight(const char *str1, const char *str2);
 
 void sort_reverse(char **text, int nLines);
 int comp_lines_reverse(const void *line1, const void *line2);
-int strcmp_reverse(const char *str1, const char *str2);
 
 void quick_sort(char *data[], int begining, int ending);
 int partition(char *data[], int begining, int ending);
-void swap_elems(char *data[], int a, int b);
+void swap_elems(char *data[], int a, int b); */
 
-void print_strings(FILE *fresult, char **text, int nLines);
+/*void print_strings(FILE *fresult, char **text, int nLines);
 void print_buf(FILE *fresult, char *buf, int sz);
-void print_result(const char *OutputFile, char *buf, char **text, int nLines, int sz);
+void print_result(const char *OutputFile, TextData *textdata);*/
 
 void free_all(char *buf, char **text);
 
-
-
-//void print_strings(char **text, int nLines);
-//void print_buf(char *buf, int sz);
+//void read_from_file(const char *InputFile,TextData *textdata);
 
 
 
 int main() {
 
-    FILE *fp = fopen( "OneginEnglish.txt" , "r" );
+    TextData textdata = {0, 0, 0, 0};
 
-    int sz = size_of_file(fp);
-    char *buf = read_data_from_file(fp, sz);
+    read_from_file("OneginEnglish.txt", &textdata);
 
-    fclose(fp);
+    print_result("OneginResult.txt", &textdata);
 
-    int nLines = number_of_symbs(buf, sz, '\n');
-    //printf("Количество строк nLines = %d\n\n", nLines);
-
-    char **text = create_pointer_arr(buf, sz, nLines);
-
-    print_result("OneginResult.txt", buf, text, nLines, sz);
-
-    free_all(buf, text);
+    free_all(textdata.buf, textdata.text);
 
     return 0;
 }
 
 
-int size_of_file(FILE *fp) {
+/*int size_of_file(FILE *fp) {
     fseek(fp, 0, SEEK_END);
     int sz = ftell(fp);
     fseek(fp, 0, SEEK_SET);
@@ -74,10 +71,11 @@ char *read_data_from_file(FILE *fp, int sz) {
     }
 
     char *buf = (char*) calloc(sz + 1, sizeof(char));
-                                                       // this is krivo!!!
+
     int nRead = fread(buf, sizeof(char), sz, fp);
     assert (nRead <= sz);
-    buf[sz] = '\n';
+
+    buf[nRead - 1] = '\n';
 
     return buf;
 }
@@ -90,14 +88,14 @@ int number_of_symbs(const char *buf, size_t len, char symb) {
             nSymbs ++;
         }
     }
-    return nSymbs + 1;
+    return nSymbs;
 }
 
 
 char **create_pointer_arr(char *buf, int sz, int nLines) {
     char **text = (char**) calloc(nLines, sizeof(char*));
     for(int i = 0; i < nLines; i++) {
-        text[i] = 0;
+        text[i] = NULL;
     }
     text[0] = buf;
     int line = 1;
@@ -110,10 +108,10 @@ char **create_pointer_arr(char *buf, int sz, int nLines) {
     }
 
     return text;
-}
+}*/
 
 
-void sort_straight(char **text, int nLines) {
+/*void sort_straight(char **text, int nLines) {
     qsort(text, nLines, sizeof(char*), comp_lines_straight);
 }
 
@@ -153,9 +151,16 @@ int comp_lines_straight(const void *line1, const void *line2) {
         else {
             return 1;
         }
-
-        if (i > end_1 && j > end_2) {
-            return 0;
+        if (i > end_1 || j > end_2) {
+            if (i > end_1 && j > end_2) {
+                return 0;
+            }
+            else if (i > end_1) {
+                return -1;
+            }
+            else {
+                return 1;
+            }
         }
     }
 }
@@ -202,19 +207,24 @@ int comp_lines_reverse(const void *line1, const void *line2) {
         else {
             return 1;
         }
-
-        if (i < 0) {
-            return -1;
+        if (i < 0 || j < 0) {
+            if (i < 0 && j < 0) {
+                return 0;
+            }
+            else if (i < 0) {
+                return -1;
+            }
+            else {
+                return 1;
+            }
         }
 
-        if (j < 0) {
-            return 1;
-        }
     }
-}
+} */
 
 
-void print_strings(FILE *fresult, char **text, int nLines) {
+
+/*void print_strings(FILE *fresult, char **text, int nLines) {
     for (int i = 0; i < nLines; ++i) {
         fprintf(fresult, "%s\n", text[i]);
     }
@@ -230,88 +240,10 @@ void print_buf(FILE *fresult, char *buf, int sz) {
             fputc(buf[i], fresult);
         }
     }
-}
+}*/
 
 
-
-int strcmp_straight(const char* str1, const char* str2) {
-    size_t end_1 = strlen(str1) - 1;
-    size_t end_2 = strlen(str2) - 1;
-
-    size_t i = 0;
-    size_t j = 0;
-
-    while (true)
-    {
-        while (i <= end_1 && (isalpha(str1[i]) == 0)) {
-            i++;
-        }
-
-        while (j <= end_2 && (isalpha(str2[j]) == 0)) {
-            j++;
-        }
-
-        if (str1[i] == str2[j]) {
-            i++;
-            j++;
-        }
-
-        else if (str1[i] < str2[j]) {
-            return -1;
-        }
-
-        else {
-            return 1;
-        }
-
-        if (i > end_1 && j > end_2) {
-            return 0;
-        }
-    }
-}
-
-/*int strcmp_reverse(const char* str1, const char* str2) {
-    size_t end_1 = strlen(str1) - 1;
-    size_t end_2 = strlen(str2) - 1;
-
-    int i = (int)end_1;
-    int j = (int)end_2;
-
-    while (true)
-    {
-        while (i >= 0 && (isalpha(str1[i]) == 0)) {
-            i--;
-        }
-
-        while (j >= 0 && (isalpha(str2[j]) == 0)) {
-            j--;
-        }
-
-        if (str1[i] == str2[j]) {
-            i--;
-            j--;
-        }
-
-        else if (str1[i] < str2[j]) {
-            return -1;
-        }
-
-        else {
-            return 1;
-        }
-
-        if (i < 0) {
-            return -1;
-        }
-
-        if (j < 0) {
-            return 1;
-        }
-    }
-} */
-
-
-void quick_sort(char **data, int begining, int ending) {
+/*void quick_sort(char **data, int begining, int ending) {
     if (begining < ending) {
         int middle = partition(data, begining, ending);
         quick_sort(data, begining, middle);
@@ -348,33 +280,51 @@ void swap_elems(char *data[], int a, int b) {
     data[a] = data[b];
     data[b] = tmp;
 }
-
+  */
 
 void free_all(char *buf, char **text) {
     free(buf);
     free(text);
 }
 
-void print_result(const char *OutputFile, char *buf, char **text, int nLines, int sz) {
+/*void print_result(const char *OutputFile, TextData *textdata) {
 
     FILE *fresult = fopen( OutputFile , "w" );
 
     fprintf(fresult, "Сортировка строк в алфавитном порядке\n\n");
-    //sort_straight(text, nLines);
-    quick_sort(text, 0, nLines - 1);
-    print_strings(fresult, text, nLines);
+    sort_straight(textdata->text, textdata->nLines);
+    //quick_sort(text, 0, nLines - 1);
+    print_strings(fresult, textdata->text, textdata->nLines);
     fprintf(fresult, "\n\n");
 
     fprintf(fresult, "Обратная сортировка\n\n");
-    sort_reverse(text, nLines);
-    print_strings(fresult, text, nLines);
+    sort_reverse(textdata->text, textdata->nLines);
+    print_strings(fresult, textdata->text, textdata->nLines);
     fprintf(fresult, "\n\n");
 
     fprintf(fresult, "Текст в оригинальном виде\n\n");
-    print_buf(fresult, buf, sz);
+    print_buf(fresult, textdata->buf, textdata->sz);
 
     fclose(fresult);
-}
+} */
+
+/*void read_from_file(const char *InputFile,TextData *textdata) {
+
+    FILE *finput = fopen(InputFile , "r" );
+
+    textdata->sz = size_of_file(finput);
+    textdata->buf = read_data_from_file(finput, textdata->sz);
+
+    fclose(finput);
+
+    textdata->nLines = number_of_symbs(textdata->buf, textdata->sz, '\n');
+    if (textdata->buf[textdata->sz - 1] != '\n') {
+        textdata->nLines ++;
+    }
+    printf("something");
+
+    textdata->text = create_pointer_arr(textdata->buf, textdata->sz, textdata->nLines);
+} */
 
 
 
